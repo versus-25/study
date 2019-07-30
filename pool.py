63802e59e203984a7bae4b0f0690d17bbb58755e
versus-25 <53166911+versus-25@users.noreply.github.com>
2019-07-30 22:22:43 +0300

    Update pool.py

--- pool.py
+++ pool.py
@@ -1,33 +1,57 @@
+#Яша плавал в бассейне размером N × M метров и устал. 
+#В этот момент он обнаружил, что находится на расстоянии x метров от одного из длинных бортиков 
+#(не обязательно от ближайшего) и y метров от одного из коротких бортиков. 
+#Какое минимальное расстояние должен проплыть Яша, чтобы выбраться из бассейна на бортик? 
+#Программа получает на вход числа N, M, x, y. 
+#Программа должна вывести число метров, которое нужно проплыть Яше до бортика.
 N = int(input("1 размер бассейна"))
 M = int(input("2 размер бассейна"))
 x = int(input("расстояние до одного из длинных бортиков"))
 y = int(input("расстояние до одного из коротких бортиков"))
-N-y M-x y x 
+
 if N >= M:#N-длинный
-    if (N - y) >= (M - x):
+    if (N - y) >= y:
         if (M - x) >= x:
-            print(x)
+            if y >= x:
+                print(x)
+            else:
+                print(y)
         else:
-            print(M - x)
+            if y >= (M - x):
+                print(M - x)
+            else:
+                print(y)
     else:
-        if (N - y) >= y:
-            print(y) 
+        if (M - x) >= x:
+            if (N - y) >= x:
+                print(x)
+            else:
+                print(N - y)
         else:
-            print(N - y)
-if N < M:#M-длинный
-    if (M - y) >= (N - x):
+            if (N - y) >= (M - x):
+                print(M - x)
+            else:
+                print(N - y)
+else:#M-длинный
+    if (M - y) >= y:
         if (N - x) >= x:
-            print(x)
+            if y >= x:
+                print(x)
+            else:
+                print(y)
         else:
-            print(N - x)
+            if y >= (N - x):
+                print(N - x)
+            else:
+                print(y)
     else:
-        if (M - y) >= y:
-            print(y) 
+        if (N - x) >= x:
+            if (M - y) >= x:
+                print(x)
+            else:
+                print(M - y)
         else:
-            print(M - y)
-else:#квадратный бассейн N = M
-    if x >= y:
-        if (N - x) > y:
-            print(y)
-        else:
-            print(N - x)
+            if (M - y) >= (N - x):
+                print(N - x)
+            else:
+                print(M - y)
